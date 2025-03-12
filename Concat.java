fadd8cdecbf3bfdd2d4a3a7670baf5ef3fa47b9c
savithrimvaishu <savithri1412005@gmail.com>
2025-03-12 15:15:07 +0530

    Add

---
+++ Concat.java
@@ -0,0 +1,7 @@
+public class Concat {
+    public static void main(String[] args) {
+        String name1="Savithri";
+        String name2="Vaishu";
+                System.out.println(name1.concat(name2));
+    }
+}
