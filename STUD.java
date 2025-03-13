b578af62cb0eb6dfb7aecae5917c426d3b79fa24
savithrimvaishu <savithri1412005@gmail.com>
2025-03-13 10:04:58 +0530

    Student list

---
+++ STUD.java
@@ -0,0 +1,15 @@
+public class STUD {
+    public static void main(String[] args)
+     {
+        String name="Savithri";
+        int mark=95;
+        char grade= 'O';
+        String subject="Maths";
+        System.out.println("Enter your name:"+name);
+        System.out.println("Enter your mark:"+mark);
+        System.out.println("Enter your grade:"+grade);
+        System.out.println("Enter your subject:"+subject);
+     }
+}
+
+ 
