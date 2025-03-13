5268d66581cb1540fe268bfe6e43d5c8ada28b38
savithrimvaishu <savithri1412005@gmail.com>
2025-03-13 14:37:40 +0530

    Examp1

---
+++ Conditional.java
@@ -0,0 +1,10 @@
+public class Conditional {
+    public static void main(String[] args)
+    {
+        int age=25;
+        if(age>=18)
+        System.out.println("She is a major");
+        else
+        System.out.println("She is a minor");
+            }
+}
