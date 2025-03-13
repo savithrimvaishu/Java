c241de18bb6064971be0965a0796b95eade99277
savithrimvaishu <savithri1412005@gmail.com>
2025-03-13 13:54:24 +0530

    Num

---
+++ Swapping2.java
@@ -0,0 +1,15 @@
+public class Swapping2 {
+    public static void main(String[] args)
+    {
+        int a=6;
+        int b=4;
+        System.out.println("value of a is" +a);
+        System.out.println("Value of b is" +b);
+        int temp=a;
+        a=b;
+        b=temp;
+        System.out.println("After Swapping");
+        System.out.println("Value of a is" +a);
+        System.out.println("Value of b is" +b);
+    }
+}
