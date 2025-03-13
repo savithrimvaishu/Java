c850e4b6e8804962a7ff9142b0639de753993d19
savithrimvaishu <savithri1412005@gmail.com>
2025-03-13 15:06:42 +0530

    Examp3

---
+++ Nested1.java
@@ -0,0 +1,16 @@
+public class Nested1 {
+    public static void main(String[] args)
+    {
+        int mark=32;
+        if(mark>=50){
+        System.out.println("Grade A");}
+        else if(mark>30){
+                System.out.println("Grade B");
+            }
+           else if(mark>=20){
+            System.out.println("Grade c");
+           }
+           
+        }
+       
+}
