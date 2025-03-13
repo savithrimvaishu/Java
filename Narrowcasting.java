9ad91a3a337db1e32f460473297f0089f6551ee0
savithrimvaishu <savithri1412005@gmail.com>
2025-03-13 10:42:26 +0530

    Example

---
+++ Narrowcasting.java
@@ -0,0 +1,9 @@
+public class Narrowcasting {
+    public static void main(String[] args)
+    {
+        double mydouble=2.33d;
+        int number=(int)mydouble;
+        System.out.println("Enter the number:"+number);
+
+    }
+}
