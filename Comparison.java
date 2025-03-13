b3b35d59175cdcc0ce8087202766a1a845be53b7
savithrimvaishu <savithri1412005@gmail.com>
2025-03-13 11:49:31 +0530

    String

---
+++ Comparison.java
@@ -0,0 +1,8 @@
+public class Comparison {
+    public static void main(String[] args)
+    {
+        String name1="mango";
+        String name2="MANGO";
+    System.out.println(name1==name2);
+    }
+  }
