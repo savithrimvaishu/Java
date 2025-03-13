4deee53e6368ce798f063052dcc210b7eab5f93e
savithrimvaishu <savithri1412005@gmail.com>
2025-03-13 12:03:51 +0530

    Eg

---
+++ Logical.java
@@ -0,0 +1,9 @@
+public class Logical {
+    public static void main(String[] args)
+    {
+        int a=35;
+        int b=45;
+        System.out.println(a > b && a == b);
+        System.out.println(a > b || a == b);
+    }
+}
