0d6c1af983c4c4fd4d6449df2bc8dc7c2ad9391a
savithrimvaishu <savithri1412005@gmail.com>
2025-03-14 10:34:03 +0530

    LoopEx2

---
+++ Lexample2.java
@@ -0,0 +1,11 @@
+public class Lexample2 {
+    public static void main(String[] args)
+    {
+        int i=1;
+        while(i<=5)
+        {
+        System.out.println(10);
+        i++;
+        }
+    }
+}
