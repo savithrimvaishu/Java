4628708b61e9f97478020f047acf61361524fc9e
savithrimvaishu <savithri1412005@gmail.com>
2025-03-14 15:40:25 +0530

    Stud list

---
+++ TestStudent.java
@@ -0,0 +1,16 @@
+class student
+    {
+    String name;
+    int id;
+}
+   class TestStudent
+   {
+    public static void main(String[] args)
+    {
+    student s1 = new student();
+    s1.id=100;
+    System.out.println(s1.id);
+    s1.name="vaishu";
+    System.out.println(s1.name);
+   }
+}
