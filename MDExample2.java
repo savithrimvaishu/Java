558cfaa7affa87c34efcff2fb66bef188fc1bc6e
savithrimvaishu <savithri1412005@gmail.com>
2025-03-14 15:05:07 +0530

    MDExample

---
+++ MDExample2.java
@@ -0,0 +1,9 @@
+public class MDExample2 {
+    public static void main(String[] args)
+    {
+        String []fruits={"Apple","Mango","Guava","Watermelon","Orange","Grapes"};
+    for(int i=0;i<=4;i++)
+    System.out.println(fruits[i]);
+    }
+
+}
