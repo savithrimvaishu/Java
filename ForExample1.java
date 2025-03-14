12c9a8e4d5b425e98a136d18e984d94205b39481
savithrimvaishu <savithri1412005@gmail.com>
2025-03-14 11:21:28 +0530

    Using forloop1

---
+++ ForExample1.java
@@ -0,0 +1,11 @@
+public class ForExample1 {
+    public static void main(String[] args){
+        for(int i=-10;i<=-5;i--){
+        if(i==-15)
+        {
+            break;
+        }
+    System.out.println(i);
+    }
+    }
+}
