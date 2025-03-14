8ff7d30bb505f5bb9efb8bfc9d75e239de19762d
savithrimvaishu <savithri1412005@gmail.com>
2025-03-14 12:19:41 +0530

    Main eg

---
+++ Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    static void mymethod() {
+        System.out.println("Welcome");
+    }
+    public static void main(String[] args)
+    {
+        mymethod();
+    }
+    }
