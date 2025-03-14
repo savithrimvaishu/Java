0169707cc45d0b1364fba3416544fb888809b299
savithrimvaishu <savithri1412005@gmail.com>
2025-03-14 10:38:05 +0530

    LoopEx3

---
+++ Lexample3.java
@@ -0,0 +1,12 @@
+
+public class Lexample3 {
+    public static void main(String[] args)
+    {
+        int i=1;
+        while(i<=10)
+        {
+        System.out.println("Cat");
+        i++;
+        }
+    }
+}
