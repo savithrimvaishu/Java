f22eb0fb0585ba643783712f2f161aebfee623fc
savithrimvaishu <savithri1412005@gmail.com>
2025-03-14 13:58:03 +0530

    Using array1

---
+++ AExample1.java
@@ -0,0 +1,10 @@
+public class AExample1 {
+    public static void main(String[] args)
+    {
+        String []fruits={"Guava","Pineapple","Papaya"};
+        {
+    System.out.println(fruits.length);
+  
+    }
+}
+}
