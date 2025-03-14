7ba2f3ce82c50ccdf6c49c1a562c97be4618b845
savithrimvaishu <savithri1412005@gmail.com>
2025-03-14 11:05:31 +0530

    LoopEx6

---
+++ Lexample6.java
@@ -0,0 +1,13 @@
+public class Lexample6 {
+    public static void main(String[] args)
+    {
+        int i=0;
+        do 
+        {
+        System.out.println("Butterfly");
+        i++;
+        }
+        while(i<=5);
+    }
+}
+  
