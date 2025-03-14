b9ad270b5c5fb97adf44a1a45c5959ca20762972
savithrimvaishu <savithri1412005@gmail.com>
2025-03-14 13:49:14 +0530

    Using array

---
+++ AExample.java
@@ -0,0 +1,13 @@
+public class AExample {
+    public static void main(String[] args)
+    {
+        int []a={1,2,3,4};
+        String []fruits={"Apple","Banana","Cherry","Mango"};
+        {
+    System.out.println(fruits[0]);
+    System.out.println(fruits[1]);
+    System.out.println(fruits[2]);
+    System.out.println(fruits[3]);
+    }
+}
+    }
