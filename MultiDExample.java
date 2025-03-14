038fa6b084654ede2b163520387785524b7b9a2d
savithrimvaishu <savithri1412005@gmail.com>
2025-03-14 14:11:51 +0530

    Using multiD

---
+++ MultiDExample.java
@@ -0,0 +1,9 @@
+public class MultiDExample {
+    public static void main(String[] args)
+    {
+        int [][] numbers={{1,2,3,4},{5,6,7,8}};
+        {
+            System.out.println(numbers[1][1]);
+        }
+    }
+}
