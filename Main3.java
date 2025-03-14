5ef6489529a793c132ee9f538bcbec64d7b32949
savithrimvaishu <savithri1412005@gmail.com>
2025-03-14 13:23:28 +0530

    Main3 eg

---
+++ Main3.java
@@ -0,0 +1,17 @@
+public class Main3 {
+    static void mymethod()
+    {
+        int mark=70;
+        if(mark>=45)
+        {
+            System.out.println("Pass");
+        }
+else{
+    System.out.println("Fail");
+}
+    }
+public static void main(String[] args)
+{
+    mymethod();
+}
+    }
