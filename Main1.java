43da11197b5ba98d9dc2f01aa86a3ef7f4359036
savithrimvaishu <savithri1412005@gmail.com>
2025-03-14 13:14:43 +0530

    Main1 eg

---
+++ Main1.java
@@ -0,0 +1,13 @@
+public class Main1 {
+    static void mymethod()
+    {
+    int a=4;
+    int b=5;
+    int c=a*b;
+    System.out.println("The value is:" +a*b);
+    }
+    public static void main(String[] args)
+    {
+        mymethod();
+    }
+    }
