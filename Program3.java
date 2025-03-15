aea2f227ed0a4b65af2191dc4c2984cdd5b2e64a
savithrimvaishu <savithri1412005@gmail.com>
2025-03-15 11:30:02 +0530

    EXProg

---
+++ Program3.java
@@ -0,0 +1,25 @@
+public class Program3 {
+    public static void main(String[] args)
+    {
+        int item=2;
+        switch(item)
+        {
+            case 1:
+            System.out.println("Black Coffee");
+            break;
+            case 2:
+            System.out.println("Green Tea");
+            break;
+            case 3:
+            System.out.println("Lemon Tea");
+            break;
+            case 4:
+            System.out.println("Ginger Tea");
+            break;
+            default:
+            System.out.println("INVALID");
+
+        }
+
+    }
+}
