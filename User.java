9d4f5986bdf05fedfce69f2c41d0e5ebcd6f3d12
savithrimvaishu <savithri1412005@gmail.com>
2025-03-15 12:11:56 +0530

    using int

---
+++ User.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class User {
+    public static void main(String[] args)
+    {
+    Scanner m = new Scanner(System.in);
+    System.out.println("Enter your name:");
+    String username=m.nextLine();
+    System.out.println("The username is:" +username);
+}
+}
