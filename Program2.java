3157288000d8dd218a1e712f29e264b37c703f2d
savithrimvaishu <savithri1412005@gmail.com>
2025-03-15 10:38:59 +0530

    Using loop

---
+++ Program2.java
@@ -0,0 +1,9 @@
+public class Program2 {
+    public static void main(String[] args)
+    {
+        int []a={150,200,350,400,550};
+        for(int i=0;i<=4;i++){
+    System.out.println(a[i]);
+    }
+}
+}
