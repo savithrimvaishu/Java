0e60f6c6045f056ea2cfdb650d13048d1546cf1b
savithrimvaishu <savithri1412005@gmail.com>
2025-03-15 10:53:29 +0530

    Using loop1

---
+++ Program1.java
@@ -0,0 +1,11 @@
+public class Program1 {
+    public static void main(String[] args)
+    {
+        int []price={30,70,40,50,80};
+        System.out.println("The price of ice is:" +price[0]);
+        System.out.println("The price of pen is:" +price[1]);
+        System.out.println("The price of note is:" +price[2]);
+        System.out.println("The price of choclate is" +price[3]);
+        System.out.println("The price of Maggie is" +price[4]);
+    }
+    }
